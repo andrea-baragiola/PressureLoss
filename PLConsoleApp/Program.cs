b549369efae8ee3404d73c6dc3545d2c83cf111a
Andrea Baragiola <andrea.baragiola@outlook.it>
2023-09-13 22:24:09 +0200

    edit

--- PLConsoleApp/Program.cs
+++ PLConsoleApp/Program.cs
@@ -4,29 +4,42 @@ using PressureLossCalculations;
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
+List<IResults> ResultsList = new List<IResults>();
+
+while (true)
+{
+    InputData inputData = GetAllInputs();
+    IResults results = new ResultsHazenWilliams(inputData);
+    ResultsList.Add(results);
+
+    Console.WriteLine("Do you want to add another pipe? (y/n)");
+    string answer = Console.ReadLine();
+    while (answer != "y" && answer !="n")
+    {
+        Console.WriteLine("Unacceptable answer. Do you want to add another pipe? (y/n)");
+        answer = Console.ReadLine();
+    }
+    if (answer == "n") break;
+}
 
 
-double pipeLenght = RetrieveDoubleValue("Pipe lenght (m):");
-double pipeDiameter = RetrieveDoubleValue("Pipe internal diameter (mm):");
-double waterFlowRate = RetrieveDoubleValue("Water flow rate (l/s):");
-double pipeSurfaceFactor = RetrieveDoubleValue("Pipe Surface Factor:");
-int numberOf90DegCurves = RetrieveIntValue("Number Of 90° Curves:");
-int numberOf45DegCurves = RetrieveIntValue("Number Of 45° Curves:");
-int numberOfTJunctions = RetrieveIntValue("Number Of T Junctions:");
 
 
-InputData inputData = new InputData
-    (pipeLenght, pipeDiameter, waterFlowRate, pipeSurfaceFactor, numberOf90DegCurves, numberOf45DegCurves, numberOfTJunctions);
-IResults results = new ResultsHazenWilliams(inputData);
 
 
 Console.Write("Tratti:".PadRight(20)); Console.Write("PdC lin.".PadRight(20)); Console.Write("PdC loc.".PadRight(20)); Console.Write("PdC tot.".PadRight(20)); Console.WriteLine();
 
-Console.Write("1:".PadRight(20));
-Console.Write(results.FrictionalPressureLoss.ToString("#.##").PadRight(20));
-Console.Write(results.LocalPressureLoss.ToString("#.##").PadRight(20));
-Console.Write(results.TotalPressureLoss.ToString("#.##").PadRight(20));
-Console.WriteLine();
+int i = 1;
+foreach (IResults results in ResultsList)
+{
+
+    Console.Write(i.ToString().PadRight(20));
+    Console.Write(results.FrictionalPressureLoss.ToString("#.##").PadRight(20));
+    Console.Write(results.LocalPressureLoss.ToString("#.##").PadRight(20));
+    Console.Write(results.TotalPressureLoss.ToString("#.##").PadRight(20));
+    Console.WriteLine();
+    i++;
+}
 
 
 
@@ -62,4 +75,19 @@ int RetrieveIntValue(string message)
         Console.WriteLine("Invalid format, try again");
         return RetrieveIntValue(message);
     }
+}
+
+InputData GetAllInputs()
+{
+    double pipeLenght = RetrieveDoubleValue("Pipe lenght (m):");
+    double pipeDiameter = RetrieveDoubleValue("Pipe internal diameter (mm):");
+    double waterFlowRate = RetrieveDoubleValue("Water flow rate (l/s):");
+    double pipeSurfaceFactor = RetrieveDoubleValue("Pipe Surface Factor:");
+    int numberOf90DegCurves = RetrieveIntValue("Number Of 90° Curves:");
+    int numberOf45DegCurves = RetrieveIntValue("Number Of 45° Curves:");
+    int numberOfTJunctions = RetrieveIntValue("Number Of T Junctions:");
+
+    InputData inputData = new InputData
+        (pipeLenght, pipeDiameter, waterFlowRate, pipeSurfaceFactor, numberOf90DegCurves, numberOf45DegCurves, numberOfTJunctions);
+    return inputData;
 }
