2ea26662863569e44d39eab3c32745792d23b0e7
Andrea Baragiola <andrea.baragiola@outlook.it>
2023-09-21 15:11:41 +0200

    fixed resultfunction reference in winform project

--- PLWinFormsApp/Utilities.cs
+++ PLWinFormsApp/Utilities.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PressureLossCalculations;
+using PressureLossCalculations.Calculations;
 using PressureLossCalculations.Models;
 
 namespace PLWinFormsApp
