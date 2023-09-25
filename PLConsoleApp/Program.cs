fd2ef7e9fa4c9a1724aa51f723ba3719e0c8cc79
Andrea Baragiola <andrea.baragiola@outlook.it>
2023-09-25 20:33:34 +0200

    added DI to console app

--- PLConsoleApp/Program.cs
+++ PLConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Globalization;
+using Microsoft.Extensions.DependencyInjection;
+using PLConsoleApp;
 using PLConsoleApp.InputHelpers;
 using PLConsoleApp.UILogic;
 using PressureLossCalculations.Models;
@@ -7,7 +9,10 @@ using PressureLossCalculations.Services;
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
-UILogic uilogic = new UILogic(new HazenWilliamsCalculator());
+var container = Startup.ConfigureService();
+var calculator = container.GetRequiredService<ICalculator>();
+
+UILogic uilogic = new UILogic(calculator);
 
 List<IResults> ResultsList = uilogic.GetInputsAndCalculate();
 
---
+++ PLConsoleApp/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using PressureLossCalculations.Models;
+using PressureLossCalculations.Services;
+
+namespace PLConsoleApp
+{
+    public class Startup
+    {
+        public static IServiceProvider ConfigureService()
+        {
+            var provider = new ServiceCollection()
+                .AddSingleton<ICalculator, HazenWilliamsCalculator>()
+                .BuildServiceProvider();
+            return provider;
+        }
+    }
+}
