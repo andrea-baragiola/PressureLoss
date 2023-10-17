af4eaf82ab0882261d3bf29bd132c1e58a230bc9
Andrea Baragiola <andrea.baragiola@outlook.it>
2023-10-17 14:21:48 +0200

    added data access project and verified on console app

---
+++ DataAccess/DbAccess/DataAccessHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using DataAccess.Models;
+using Dapper;
+
+namespace DataAccess.DbAccess
+{
+    public class DataAccessHelper
+    {
+        private readonly string connectionString = "Server=.;Database=People;User Id=sa;Password=Password1@;";
+        public async Task<IEnumerable<Person>> GetPeople()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                var output = await connection.QueryAsync<Person>("select * from PeopleTable");
+
+                return output;
+            }
+        }
+    }
+}
--- PLConsoleApp/Program.cs
+++ PLConsoleApp/Program.cs
@@ -5,9 +5,19 @@ using PLConsoleApp.InputHelpers;
 using PLConsoleApp.UILogic;
 using PressureLossCalculations.Models;
 using PressureLossCalculations.Services;
+using DataAccess.DbAccess;
+using DataAccess.Models;
+using System.Collections.Generic;
 
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+DataAccessHelper dataAccessGHelper = new DataAccessHelper();
+IEnumerable<Person> peopleList = await dataAccessGHelper.GetPeople();
+foreach (Person person in peopleList)
+{
+    Console.WriteLine(person.Name);
+}
 
 var container = Startup.ConfigureService();
 
